8376c8f8aa556c1dcc1804cf56652ccb675f41c8
Janio Rui <bruiz@popoyan.com>
2023-10-05 23:08:47 -0600

    auto modified files

--- parent_tabdata.php
+++ parent_tabdata.php
@@ -6,7 +6,7 @@
 $parent_tab_info_array=array(1=>'My Home Page',2=>'Marketing',3=>'Sales',4=>'Support',5=>'Analytics',6=>'Inventory',7=>'Tools',8=>'Settings');
 
 
-$parent_child_tab_rel_array=array(1=>array(3,9,28,),2=>array(26,6,4,28,7,9,8,),3=>array(7,6,4,2,20,22,23,19,8,9,41,41,),4=>array(13,15,6,4,8,28,9,33,33,45,45,46,46,47,47,),5=>array(25,1,),6=>array(14,18,19,21,22,20,23,34,34,38,38,),7=>array(24,27,8,30,30,36,36,43,43,44,44,24,27,44,43,50,50,51,51,),8=>array(49,49,),);
+$parent_child_tab_rel_array=array(1=>array(3,),2=>array(26,6,4,7,),3=>array(7,6,4,),4=>array(6,4,),5=>array(1,),6=>array(18,),7=>array(44,44,44,),8=>array(49,49,),);
 
 
 
